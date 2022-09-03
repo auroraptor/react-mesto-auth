49a1a1a8ce574561114ef5e80a0a33b1ba578a62
auroraptor <interestingbutno@yandex.ru>
2022-09-03 20:30:38 +0300

    feat: add state

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -7,10 +7,12 @@ function EditProfilePopup(props) {
   const currentUser = useContext(CurrentUserContext);
   const [ name, setName ] = useState('');
   const [ description, setDescription] = useState('');
+  const [ buttonTextContent, setButtonTextContent] = useState("Сохранить")
 
   useEffect(() => {
     setName(currentUser.name);
     setDescription(currentUser.about);
+    setButtonTextContent("Сохранить")
   }, [currentUser, props.isOpen])
 
   // Нужно следить за isOpen (за состоянием открытия), чтобы вставлять в инпуты данные пользователя, иначе, если мы удалим информацию из инпутов и просто закроем попап, то при следующем открытии инпуты будут пустые (без данных пользователя)
@@ -26,6 +28,7 @@ function EditProfilePopup(props) {
 
   function handleSubmit(evt) {
     evt.preventDefault();
+    setButtonTextContent("Сохранение")
 
     props.onUpdateUser({
       name,
@@ -34,7 +37,7 @@ function EditProfilePopup(props) {
   }
 
   return (
-    <PopupWithForm name="edit-profile" title="Редактировать профиль" onSubmit={handleSubmit} {...props} buttonTextContent="Сохранить">
+    <PopupWithForm name="edit-profile" title="Редактировать профиль" onSubmit={handleSubmit} {...props} buttonTextContent={buttonTextContent}>
      <Input type="text" id="name" name="name" placeholder="Имя" minLength="2" maxLength="40" value={name} onChange={handleNameChange}></Input>
      <Input type="text" id="about" name="about" placeholder="О себе" minLength="2" maxLength="200" value={description} onChange={handleDescriptionChange}></Input>
   </PopupWithForm>
