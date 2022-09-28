85daf14b96c4b76fc1ff3b073fc605e6c8e055fa
auroraptor <interestingbutno@yandex.ru>
2022-09-28 12:22:36 +0300

    fix: format

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,36 +6,63 @@ import Input from "./Input";
 import InfoTooltip from "./InfoTooltip";
 
 function Register(props) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     props.onRegister(email, password);
-    setEmail('');
-    setPassword('');
-  }
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <>
-    <Header link="/sign-in" text="Войти" elem={<Login />}/>
-    <form
-    className={`form_theme_white register-form`}
-    name="register"
-    onSubmit={handleSubmit}>
-      <label className="heading__register">Регистрация</label>
-          <Input type="email" id="email" name="email"
-            placeholder="Email"  minLength="false" maxLength="false" value={email} onChange={e => setEmail(e.target.value)}/>
-          <Input type="password" id="password" name="password"
-            placeholder="Пароль" minLength="8" maxLength="40" value={password} onChange={e => setPassword(e.target.value)}/>
-        <button type="submit" className="form__submit-button_theme_white">Зарегестрироваться</button>
-    </form>
-    <div className="help-text">
-      <span>Уже зарегестрированы? </span>
-      <Link className="help-text__link" to="/sign-in">Войти</Link>
-    </div>
-    <InfoTooltip {...props} onClick={() => {props.closeInfoTooltip(false)}}/>
-    </>)
+      <Header link="/sign-in" text="Войти" elem={<Login />} />
+      <form
+        className={`form_theme_white register-form`}
+        name="register"
+        onSubmit={handleSubmit}
+      >
+        <label className="heading__register">Регистрация</label>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          minLength="false"
+          maxLength="false"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Пароль"
+          minLength="8"
+          maxLength="40"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className="form__submit-button_theme_white">
+          Зарегестрироваться
+        </button>
+      </form>
+      <div className="help-text">
+        <span>Уже зарегестрированы? </span>
+        <Link className="help-text__link" to="/sign-in">
+          Войти
+        </Link>
+      </div>
+      <InfoTooltip
+        {...props}
+        onClick={() => {
+          props.closeInfoTooltip(false);
+        }}
+      />
+    </>
+  );
 }
 
 export default Register;
