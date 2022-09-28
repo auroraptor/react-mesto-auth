d23b40474264c45fba312e29c49de09fdc1c55e1
auroraptor <interestingbutno@yandex.ru>
2022-09-28 12:59:14 +0300

    fix: change SCC property

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,19 +1,29 @@
 import React from "react";
-import '../index.css';
+import "../index.css";
 
-function ImagePopup({card, onClose}) {
-
-return (
-  <div className={`popup image-zoomed-popup ${card && `popup_opened`}`}>
+function ImagePopup({ card, onClose }) {
+  return (
+    <div
+      className={`popup popup_with_image image-zoomed-popup ${
+        card && `popup_opened`
+      }`}
+    >
       <div className="popup__yet-another-container">
-      <button className="popup__close-icon popup__close-button"  onClick={onClose}></button>
-      <figure className="popup__figure">
-        <img src={card?.link} alt={card && `Изображение ${card.name}`} className="popup__image" />
-        <figcaption className="popup__caption">{card?.name}</figcaption>
-      </figure>
+        <button
+          className="popup__close-icon popup__close-button"
+          onClick={onClose}
+        ></button>
+        <figure className="popup__figure">
+          <img
+            src={card?.link}
+            alt={card && `Изображение ${card.name}`}
+            className="popup__image"
+          />
+          <figcaption className="popup__caption">{card?.name}</figcaption>
+        </figure>
       </div>
     </div>
-)
+  );
 }
 
-export default ImagePopup
+export default ImagePopup;
