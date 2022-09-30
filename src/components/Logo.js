1799e18608c0a63a551184ce7de891c3be5c964d
auroraptor <interestingbutno@yandex.ru>
2022-09-30 14:51:50 +0300

    feat: add link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,9 @@
 import {React} from "react";
+import { Link } from "react-router-dom";
 import '../index.css';
 
 const Logo = () => {
-  return <div className="logo header__logo"/>
+  <Link to="/react-mesto-auth/"><div className="logo header__logo"/></Link>
 }
 
 export default Logo;
