38ac111abd50cc12ffe12417e1b1d4f5d123ddfa
auroraptor <interestingbutno@yandex.ru>
2022-10-01 13:11:08 +0300

    refactoring

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,8 @@
-import { React } from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
 
 const Logo = () => {
-  return (
-    <Link to="/react-mesto-auth/">
-      <div className="logo header__logo" />
-    </Link>
-  );
+  return <Link to="/react-mesto-auth/" className="logo header__logo" />
 };
 
 export default Logo;
