c58153697234b9125e58b2986927256e505bbc5a
auroraptor <interestingbutno@yandex.ru>
2022-11-07 22:18:03 +0300

    fix: исправить потерянную зависимость + деплой

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect} from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./Login";
